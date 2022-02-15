d4882716a515f92690092243ca705adbca4313f6
Prathamesh Karambelkar <pkarambelkar.2002@gmail.com>
2022-02-15 19:23:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#College Forum
+
+Made By :
+
+Pranit Patil
+Sarvesh Rane
+Prathamesh Karambelkar
+Prem Shiravale
---
+++ app/src/main/java/com/psp/collegeforum/ui/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.psp.collegeforum.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.psp.collegeforum.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
